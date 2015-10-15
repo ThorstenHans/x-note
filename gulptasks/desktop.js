e8469ac31daea364b484c337b2d1cedd5665b7bb
Thorsten Hans <thorsten.hans@gmail.com>
2015-10-15 16:44:18 +0200

    added icons for desktop build

--- gulptasks/desktop.js
+++ gulptasks/desktop.js
@@ -5,12 +5,23 @@
 
         gulp.task('build:desktop', function(done) {
             tasks.inSequence('private:build', 'private:app:package', 'private:build:nw', done);
-        })
+        });
+
+
         gulp.task('private:build:nw', function(done) {
+
+            var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
             var nw = new tasks.NwBuilder({
                 files: 'dist/**/*',
+                cacheDir: tasks.path.join(homeDir, '.cache'),
+                buildDir: tasks.path.join(process.cwd(), "desktop-build"),
                 version: '0.12.3',
-                platforms: ['osx64']
+                platforms: ['win32', 'win64', 'osx64', 'linux32', 'linux64'],
+                macIcns: 'assets/x-note.icns',
+                winIco: 'assets/x-note.ico'
+                // uncomment the previous row if you're buiding the app on windows or if you've followed this guide
+                // https://github.com/nwjs/nw.js/wiki/Icons
+                
             });
 
             nw.build();
