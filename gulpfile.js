f1aeed9a9eae070a6e001721934075f2b5ce882c
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-19 17:07:54 +0100

    gulp-as-a-service integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 (function(require){
     'use strict';
-    var userConfig = {
-        addOns: {
-            "gulp-less" : {}
-        },
-        sources: {
-            appStyles : ['src/styles/**.less']
-        }
-    };
-    
-    require('xplatform-build')(userConfig);
+     
+    require('gulp-as-a-service')();
 
 })(require);
