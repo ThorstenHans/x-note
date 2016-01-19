be0148b67baf7baaab0bdc37c9869829b46a44f7
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-19 18:10:13 +0100

    implemented clear task and added as dependency for default

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    cssmin = require('gulp-cssmin');
+    cssmin = require('gulp-cssmin'),
+    del = require('del');
 
+gulp.task('clear', function(done){
+    del.sync('dist/**/*', {force:true});
+    done();
+});
 
-gulp.task('default', function(){
+gulp.task('default', ['clear'] ,function(){
   return gulp.src('src/styles/**/*.css')
     .pipe(concat('app.min.css'))
     .pipe(cssmin())
