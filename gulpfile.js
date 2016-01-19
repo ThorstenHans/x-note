a3fb33ab5d78eec700459410dbab276f44640cff
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-19 18:05:56 +0100

    implemented gulp-cssmin for default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin');
 
 
 gulp.task('default', function(){
   return gulp.src('src/styles/**/*.css')
     .pipe(concat('app.min.css'))
+    .pipe(cssmin())
     .pipe(gulp.dest('dist/styles'));
 });
