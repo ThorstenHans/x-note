d384ccfbd655f0ce9611ad5bd7bce87cefb16f19
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-19 18:15:37 +0100

    added run-sequence and extracted build-app-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     cssmin = require('gulp-cssmin'),
-    del = require('del');
+    del = require('del'),
+    runSequence = require('run-sequence');
 
-gulp.task('clear', function(done){
-    del.sync('dist/**/*', {force:true});
+gulp.task('clear', function(done) {
+    del.sync('dist/**/*', {
+        force: true
+    });
     done();
 });
 
-gulp.task('default', ['clear'] ,function(){
-  return gulp.src('src/styles/**/*.css')
-    .pipe(concat('app.min.css'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('dist/styles'));
+gulp.task('build-app-css', function() {
+    return gulp.src('src/styles/**/*.css')
+        .pipe(concat('app.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('default', function(done) {
+    return runSequence('clear', ['build-app-css'], done);
 });
