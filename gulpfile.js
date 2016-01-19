2d3ff69e6c3104fe8393d7800aa943824212ef4c
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-19 18:04:59 +0100

    implemented gulp-concat for default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
 
 
 gulp.task('default', function(){
   return gulp.src('src/styles/**/*.css')
+    .pipe(concat('app.min.css'))
     .pipe(gulp.dest('dist/styles'));
 });
